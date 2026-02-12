c54b39da049d75279715829cf26e3e0b88ffc7f0
joseyvanaarsen <joseyvanaarsen@gmail.com>
2026-02-12 01:52:22 +0100

    Added XML-documentation to tests

--- tests/Kanban.Tests/UnitTest1.cs
+++ tests/Kanban.Tests/UnitTest1.cs
@@ -1,7 +1,13 @@
 ﻿namespace Kanban.Tests;
 
+/// <summary>
+/// Startup class for test project.
+/// </summary>
 public class UnitTest1
 {
+    /// <summary>
+    /// Startup method for test project.
+    /// </summary>
     [Fact]
     public void Test1()
     {
