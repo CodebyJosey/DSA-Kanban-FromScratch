9a00bb0716e5b666fa0cba9eb1a54e532e589eb2
joseyvanaarsen <joseyvanaarsen@gmail.com>
2026-02-12 01:56:32 +0100

    Added enums

---
+++ src/Kanban.Domain/Enums/TaskPriority.cs
@@ -0,0 +1,22 @@
+namespace Kanban.Domain.Enums;
+
+/// <summary>
+/// The priority of a task item.
+/// </summary>
+public enum TaskPriority
+{
+    /// <summary>
+    /// The priority for this task is low.
+    /// </summary>
+    Low,
+
+    /// <summary>
+    /// The priority for this task is normal.
+    /// </summary>
+    Normal,
+
+    /// <summary>
+    /// The priority for this task is high.
+    /// </summary>
+    High
+}
