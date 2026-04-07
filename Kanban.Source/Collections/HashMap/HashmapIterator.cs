e81fa75915b2688b40e4723e02d6125e8d5c337d
ArdyVisser <1061327@hr.nl>
2026-04-07 13:22:29 +0200

    hashmap iterator done

--- Kanban.Source/Collections/HashMap/HashmapIterator.cs
+++ Kanban.Source/Collections/HashMap/HashmapIterator.cs
@@ -9,12 +9,48 @@ namespace Kanban.Source.Collections.HashMap;
 
 public class HashmapIterator<T> : IMyIterator<T>
 {
-    /// <inheritdoc/>
-    public bool HasNext() => throw new NotImplementedException();
+    private readonly T?[] _items;
+    private readonly int _count;
+    private int _index;
+
+    /// <summary>
+    /// constructor for the hashmap iterator
+    /// </summary>
+    /// <param name="items"></param>
+    /// <param name="count"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public HashmapIterator(T?[] items, int count)
+    {
+        _items = items ?? throw new ArgumentNullException(nameof(items));
+        _count = count;
+        _index = 0;
+    }
 
     /// <inheritdoc/>
-    public T Next() => throw new NotImplementedException();
+    public bool HasNext()
+    {
+        while (_index < _count && _items[_index] is null)
+        {
+            _index++;
+        }
+
+        return _index < _count;
+    }
 
     /// <inheritdoc/>
-    public void Reset() => throw new NotImplementedException();
+    public T Next()
+    {
+        if (!HasNext())
+        {
+            throw new InvalidOperationException("No more elements.");
+        }
+
+        return _items[_index++]!;
+    }
+
+    /// <inheritdoc/>
+    public void Reset()
+    {
+        _index = 0;
+    }
 }
